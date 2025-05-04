1c13392d8a5635ba37a79db89cbabb8b289f4349
Izabela <izabela.m.alfenas@gmail.com>
2025-05-04 15:04:23 -0300

    throw error if psql env var is missing

--- src/providers/database/postgres.ts
+++ src/providers/database/postgres.ts
@@ -1,9 +1,15 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 
+const { PSQL_URL } = process.env
+
+if(!PSQL_URL) {
+  throw new Error("Required environment variable missing: PSQL_URL");
+}
+
 const database = drizzle({
   connection: {
-    connectionString: process.env.PSQL_URL!,
+    connectionString: PSQL_URL,
   },
 });
 
